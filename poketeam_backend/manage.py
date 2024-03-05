a0345cc48c2fe85009b5d83230e74a2f61a221e4
Dylan <dylan.okyere@gmail.com>
2024-03-05 12:01:35 +0100

    ready for deployment

--- poketeam_backend/manage.py
+++ poketeam_backend/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poketeam_backend.settings')
+    settings_module = 'poketeam_backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'poketeam_backend.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
